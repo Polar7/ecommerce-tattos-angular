853a6a25f3c0997cadf53e26714702f401b1a0f3
Jean Lozano <michecoanexa52@gmail.com>
2022-05-09 12:43:24 -0500

    Creacion de navbar y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component:},
+  {path: 'productos', component:},
+  {path: 'productos/tatuajes', component:},
+  {path: 'productos/joyas', component:},
+  {path: 'productos/camisetas', component:},
+  {path: 'contacto', component:}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
